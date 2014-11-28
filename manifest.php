a2a646d12357cc7db487e22ddbffc1ef38c16cee
Pisarenco Gheorghe <pisarenco.gh@gmail.com>
2014-11-28 09:10:09 +0200

    New version: 1.0.1

--- manifest.php
+++ manifest.php
@@ -6,7 +6,7 @@ $manifest = array();
 
 $manifest['name']        = __( 'Breadcrumbs', 'fw' );
 $manifest['description'] = __( 'Creates a simplified navigation menu for the pages that can be placed anywhere in the theme. This will make navigating around the website much easier.', 'fw' );
-$manifest['version'] = '1.0.0';
+$manifest['version'] = '1.0.1';
 $manifest['display'] = true;
 $manifest['standalone'] = true;
 
