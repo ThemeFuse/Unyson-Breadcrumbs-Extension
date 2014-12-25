f3241206ad3d61a4a1b01b4f09104701b53523e0
moldcraft <moldcraft@email.com>
2014-12-25 16:16:00 +0200

    Updated description

--- manifest.php
+++ manifest.php
@@ -5,7 +5,7 @@
 $manifest = array();
 
 $manifest['name']        = __( 'Breadcrumbs', 'fw' );
-$manifest['description'] = __( 'Creates a simplified navigation menu for the pages that can be placed anywhere in the theme. This will make navigating around the website much easier.', 'fw' );
+$manifest['description'] = __( 'Creates a simplified navigation menu for the pages that can be placed anywhere in the theme. This will make navigating the website much easier.', 'fw' );
 $manifest['version'] = '1.0.3';
 $manifest['display'] = true;
 $manifest['standalone'] = true;
